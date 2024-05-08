3d23b99233399359a52fe96c93eb25223ad4cd72
Davlatov Barot <davlatovbarot@Davlatovs-MacBook-Pro.local>
2024-05-08 16:22:10 +0500

    download

---
+++ user/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+import uuid, random, string
+
+def generate_random_string(length=7):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for _ in range(length))
+
+class Profile(models.Model):
+    email = models.EmailField(max_length=200, blank=False, null=False)
+    password = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    surname = models.CharField(max_length=250, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='images/', null=True, blank=True)
+    referal_link = models.CharField(max_length=8, default=generate_random_string, unique=True, editable=False)
+    number_people = models.IntegerField(default=0)
+    balance_netbo = models.FloatField(default=0.0)
+    wallet_id_netbo = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    is_identified = models.BooleanField(default=False,null=True, blank=True)
+    is_verified = models.IntegerField(null=True, blank=True)
+    is_archived = models.IntegerField(null=True, blank=True)
+    mac_address = models.CharField(max_length = 200, blank=True, null=True)
+    friend_referal_link = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.username
+    
+
+
+class Transaction(models.Model):
+    balance_netbo = models.FloatField(default=0.0)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created_at = models.IntegerField()
+
+
+
+
+class Identified(models.Model):
+    fullname = models.CharField(max_length=255)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    birthday = models.CharField(max_length=250)
+    serial_document = models.CharField(max_length=255)
+    id_image = models.ImageField(upload_to='identified/')
+    address_image = models.ImageField(upload_to='identified/')
+    selfie_image = models.ImageField(upload_to='identified/')
+    is_identified = models.BooleanField(null=True, blank=True)
+
+
+class MoneyOut(models.Model):
+    wallet_addres = models.CharField(max_length=200)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    balance_netbo = models.FloatField(default=0.0)
+    is_identified = models.BooleanField(null=True, blank=True)
+    created_at = models.IntegerField()
+
+
+
+class Strength(models.Model):
+    level1 = models.FloatField(default=0)
+    number_people1 = models.IntegerField(default=0)
+    money_out = models.BooleanField(default=True)
+
+
+    
+
---
+++ user/views.py
@@ -0,0 +1,460 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status, generics
+from .models import Profile, Transaction, Identified, MoneyOut, Strength
+from drf_yasg.utils import swagger_auto_schema
+from .serialazers import ProfileSerializer, ProfilesingupSerialazer, ProfileLoginserialazer, UpdateProfileSerializer, ProfileRefeleshSerialazer,VerificationCodeserialazer ,GMProfileserialazer, UpdatePasswordSerializer, Tranzaktionserialazer, UpdateEmPsSerializer, MoneyOutserialazer, CreatMoneyOutserialazer, IdentifiedSerializer, StrengthSerialazer
+import time, calendar
+from django.db.models import Sum
+import random, string
+from datetime import datetime
+from datetime import date, timedelta
+from django.shortcuts import get_list_or_404, get_object_or_404
+
+
+# gmail######
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+globals
+code_lis = {}
+
+def generate_random_string(length=10):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for _ in range(length))
+
+def send_email(subject, body, to_email):
+    # Gmail pochtangiz va parolingizni kiriting
+    gmail_user = 'netboxollc@gmail.com'
+    gmail_password = "rfqdwyoszfrfoczl"
+
+    # Xabar tayyorlash
+    message = MIMEMultipart()
+    message['From'] = gmail_user
+    message['To'] = to_email
+    message['Subject'] = subject
+
+    # Xabarning matnini qo'shish
+    message.attach(MIMEText(body, 'html'))
+
+    # SMTP serveriga ulanish
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(gmail_user, gmail_password)
+
+        # Xabarni yuborish
+        server.sendmail(gmail_user, to_email, message.as_string())
+#####
+
+
+@swagger_auto_schema(method='PATCH', operation_description="Tiklamoqchi bo'lgan profilning ID sini kirting!")
+@api_view(['PATCH'])
+def send_otp(request, email):
+    if request.method == 'PATCH':
+        try:
+            # Agar email bo'yicha profili topish mumkin bo'lsa
+            profile = Profile.objects.get(email=email)
+        except Profile.DoesNotExist:
+            try:
+                # Agar email bo'yicha topilmagan bo'lsa, username bo'yicha izlash
+                profile = Profile.objects.get(username=email)
+            except Profile.DoesNotExist:
+                # Agar username bo'yicha ham topilmagan bo'lsa, xato qaytarish
+                return Response({'message': -2 }, status=status.HTTP_400_BAD_REQUEST)
+        six_digit_number = generate_random_string()
+        gmail = str(profile.email)
+        massag =f"""
+        <html>
+        <head>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    line-height: 1.6;
+                    color: #333;
+                }}
+                .password-message {{
+                    max-width: 600px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #f4f4f4;
+                    border-radius: 10px;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="password-message">
+                <p>Password Recovery - MinerUP</p>
+                <p>Your new login password: <strong>{six_digit_number}</strong></p>
+                <p>After that, you can only log in with this password.</p>
+                <p>If this is not you, please contact us immediately.</p>
+            </div>
+        </body>
+        </html>
+    """
+        send_email("Password",massag, gmail)
+        profile.password = six_digit_number
+        profile.save()
+        return Response({'message': 1 },status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1 },status=status.HTTP_400_BAD_REQUEST)
+        
+@swagger_auto_schema(method='PATCH', request_body=VerificationCodeserialazer, operation_description="Tiklamoqchi bo'lgan profilning ID sini kirting!")
+@api_view(['PATCH'])
+def confirmation_otp(request):
+    if request.method == 'PATCH':
+        code = request.data.get("code")
+        if code in code_lis.keys():
+            del code_lis[code]
+            return Response({'message': 1 }, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': -2 },status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        return Response({'message': -1 },status=status.HTTP_400_BAD_REQUEST)
+    
+
+    
+@swagger_auto_schema(method='PATCH', request_body=UpdatePasswordSerializer, operation_description="Parolni o'zgartirish uchun so'rov")
+@api_view(['PATCH'])
+def update_password(request, email):
+    profile = get_object_or_404(Profile, email=email)
+    serializer = UpdatePasswordSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    new_password = serializer.validated_data['password']
+    profile.password = new_password
+    profile.save()
+    return Response({'message': 1 }, status=status.HTTP_200_OK)
+    
+class UpdateProfileAPIView(generics.UpdateAPIView):
+    serializer_class = UpdateProfileSerializer
+    queryset = Profile.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        new_username = serializer.validated_data.get('username')
+        if Profile.objects.filter(username=new_username).exclude(id=instance.id).exists():
+            return Response({'message': -4}, status=status.HTTP_400_BAD_REQUEST)
+        self.perform_update(serializer)
+        instance.refresh_from_db()
+        return Response({'message': 1, "profile": ProfileSerializer(instance).data}, status=status.HTTP_200_OK)
+    
+@swagger_auto_schema(method='PATCH', request_body=UpdateEmPsSerializer, operation_description="Yangilamaoqchi bo'lgan Profilening ID sini kirting")
+@api_view(['PATCH'])
+def update_email_password(request, pk):
+    profile = get_object_or_404(Profile, id=pk)
+    data = UpdateEmPsSerializer(instance=profile, data=request.data)
+    data.is_valid(raise_exception=True)
+    new_email = data.validated_data.get('email')
+    if Profile.objects.filter(email=new_email).exclude(id=pk).exists():
+        return Response({'message': -4}, status=status.HTTP_400_BAD_REQUEST)
+    data.save()
+    profile.refresh_from_db()
+    serializer = ProfileSerializer(profile)
+    return Response({'message': 1, "profile": serializer.data}, status=status.HTTP_200_OK)
+
+@swagger_auto_schema(method='PATCH', operation_description="O'chirmoqchi bo'lgan Profileni ID sini kirting")
+@api_view(['PATCH'])
+def archive_account(request, pk):
+    if request.method == 'PATCH':
+        profile = Profile.objects.get(id=pk)
+        is_data = int(time.time())
+        profile.is_archived = is_data
+        profile.save()
+        return Response({'message': 1}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1}, status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(method='PATCH', operation_description="O'chirmoqchi bo'lgan Profileni ID sini kirting")
+@api_view(['PATCH'])
+def verify_email(request, pk):
+    if request.method == 'PATCH':
+        try:
+            profile = Profile.objects.get(id=pk)
+        except:
+            return Response({'message': -2}, status=status.HTTP_400_BAD_REQUEST)
+        is_data = int(time.time())
+        profile.is_verified = is_data
+        profile.save()
+        return Response({'message': 1}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1}, status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(method='POST', request_body=ProfilesingupSerialazer, operation_description="Malumotlarni kirting")
+@api_view(['POST'])
+def signup(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+    # Profil ma'lumotlarini bir marta olish
+    existing_profiles = Profile.objects.filter(username=username) | Profile.objects.filter(email=email)
+    # Profil ma'lumotlarini tekshirish
+    if existing_profiles.exists():
+        return Response({'message': -2}, status=status.HTTP_400_BAD_REQUEST)
+    serializer = ProfileSerializer(data=request.data)
+    # Serializer ma'lumotlarini tekshirish
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message': 1, "profile": serializer.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@swagger_auto_schema(methods='GET')
+@api_view(['GET'])
+def get_profile(request):
+    if request.method == 'GET':
+        profile = Profile.objects.all()
+        serializer = ProfileSerializer(profile, many=True)
+        return Response({'message': 1,"profile":serializer.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(methods='GET')
+@api_view(['GET'])
+def get_profile_id(request, pk):
+    if request.method == 'GET':
+        profile = Profile.objects.get(id=pk)
+        serializer = ProfileSerializer(profile)
+        us = profile.username
+        taim = date.today()
+        s = 0
+        tr = Transaction.objects.filter(username=us)
+        for i in tr:
+            if i.created_at == taim:
+                s += 1
+        return Response({'message': 1,"profile":serializer.data, "ad_count":s}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(methods='GET')
+@api_view(['GET'])
+def get_profile_username(request, username):
+    if request.method == 'GET':
+        profile = Profile.objects.get(username=username)
+        serializer = ProfileSerializer(profile)
+        us = profile.username
+        taim = date.today()
+        s = 0
+        tr = Transaction.objects.filter(username=us)
+        for i in tr:
+            if i.created_at == taim:
+                s += 1
+        return Response({'message': 1,"profile":serializer.data, "ad_count":s}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+    
+@swagger_auto_schema(method='POST', request_body=ProfileLoginserialazer, operation_description="Malumotlarni kirting")
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    try:
+        profile = Profile.objects.get(username=username)
+    except:
+        return Response({'message': -2}, status=status.HTTP_400_BAD_REQUEST)   
+    if profile.password == password:
+        profile_serializer = ProfileSerializer(profile)
+        return Response({'message': 1, "profile":profile_serializer.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -2}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+@swagger_auto_schema(method='DELETE', operation_description="O'chirmoqchi bo'lgan Profileni ID sini kirting")
+@api_view(['DELETE'])
+def delete_profile(request, pk):
+    if request.method == 'DELETE':
+        try:
+            praduct = Profile.objects.get(id=pk)
+        except:
+            return Response({'message': -2 },status=status.HTTP_400_BAD_REQUEST)
+        praduct.delete()
+        return Response({'message':1},status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1 },status=status.HTTP_400_BAD_REQUEST)
+    
+@swagger_auto_schema(method='PATCH', request_body=ProfileRefeleshSerialazer, operation_description="Referal_link")
+@api_view(['PATCH'])
+def activate_referral_link(request, pk):
+    if request.method == 'PATCH':
+        referal = request.data.get('referal_link')
+        try:
+            frend = Profile.objects.get(referal_link=referal)
+        except:
+            return Response({'message': -2}, status=status.HTTP_400_BAD_REQUEST)
+        profile = Profile.objects.get(id=pk)
+        if frend.is_identified == True:
+            link = frend.referal_link
+            profile.friend_referal_link = link
+            profile.save()
+            return Response({'message': 1}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': -3}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+    
+    
+@swagger_auto_schema(method='PATCH', operation_description="Ball berladigan profile ID sini kirting")
+@api_view(['PATCH'])
+def ad_reward(request, pk):
+    try:
+        profile = Profile.objects.get(id=pk)
+    except:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+    strength = Strength.objects.get(id = 1)
+    username_id = profile.id
+    taim1 = int(time.time())
+    if profile.number_people < strength.number_people1:
+        nom = strength.netbo + (strength.level1 * profile.number_people)
+    elif profile.number_people < strength.number_people2:
+        nom = strength.netbo + (strength.level2 * profile.number_people)
+    else:
+        nom = strength.netbo + (strength.level3 * profile.number_people)
+    profile.balance_netbo += nom
+    profile.save()
+    data = {"user":username_id, 'balance_netbo':nom, "created_at":taim1}
+
+    tran = Tranzaktionserialazer(data=data)
+    profile.last_mining = int(time.time())
+    profile.save()
+    tran.is_valid(raise_exception=True)
+    tran.save()
+    return Response({'message': 1, "transaction":tran.data},status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def balance_history(request, pk):
+    current_timestamp = int(time.time())
+    current_date = datetime.utcfromtimestamp(current_timestamp).date()
+
+    dey_sum = 0
+    days_in_month = calendar.monthrange(current_date.year, current_date.month)[1]
+    moon_sum = [0] * days_in_month
+    week_sum = [0] * 7
+    profile = Profile.objects.get(id=pk)
+    username_id = profile.id
+
+    all_transactions = Transaction.objects.filter(user=username_id)
+
+    # Kunlik tranzaksiyalar
+    daily_transactions = all_transactions.filter(
+        created_at__gte=current_timestamp - 86400,
+        created_at__lt=current_timestamp
+    )
+    dey_sum = daily_transactions.aggregate(Sum('balance_netbo'))['balance_netbo__sum'] or 0
+
+    # Haftalik tranzaksiyalar
+    weekly_transactions = all_transactions.filter(
+        created_at__gte=current_timestamp - (86400 * 7),
+        created_at__lt=current_timestamp
+    )
+    week_sum = [0] * 7  # Reset week_sum
+    for transaction in weekly_transactions:
+        transaction_date = datetime.utcfromtimestamp(transaction.created_at).date()
+        day_of_week = transaction_date.weekday()
+        week_sum[day_of_week] += transaction.balance_netbo
+
+    # Oylik tranzaksiyalar
+    oylik_transactions = all_transactions.filter(
+        created_at__gte=current_timestamp - (86400 * days_in_month),
+        created_at__lt=current_timestamp
+    )
+    moon_sum = [0] * days_in_month  # Reset moon_sum
+    for transaction in oylik_transactions:
+        transaction_date = datetime.utcfromtimestamp(transaction.created_at).date()
+        day_of_month = transaction_date.day - 1
+        moon_sum[day_of_month] += transaction.balance_netbo
+
+    return Response({'message': 1, 'daily': dey_sum, "weekly": week_sum, 'monthly': moon_sum}, status=status.HTTP_200_OK)
+    
+    
+
+@swagger_auto_schema(method='POST', request_body=IdentifiedSerializer, operation_description="Malumotlarni kirting")
+@api_view(['POST'])
+def upload_image(request, pk):
+    serializer = IdentifiedSerializer(data=request.data)
+    if  serializer.is_valid():
+        profile = Profile.objects.get(id=pk)
+        profile.is_identified = None
+        profile.save()
+        serializer.save(user_id=pk)
+        return Response({'message': 1, 'data': serializer.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+
+
+@swagger_auto_schema(methods='GET')
+@api_view(['GET'])
+def get_identified_id(request, pk):
+    if request.method == 'GET':
+        profile = Identified.objects.get(id=pk)
+        serializer = IdentifiedSerializer(profile)
+        return Response({'message': 1,"Identified":serializer.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(method='POST', request_body=CreatMoneyOutserialazer, operation_description="Malumotlarni kirting")
+@api_view(['POST'])
+def moneyout(request, pk):
+    stf = get_object_or_404(Strength, id = 1).money_out
+    if stf == True:
+        try:
+            profile = Profile.objects.get(id=pk)
+        except:
+            return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
+        taim = int(time.time())
+        wallet_address = request.data.get('wallet_addres')
+        balance_netboo = request.data.get('balance_netbo')
+        data = {"wallet_addres":wallet_address, "user":profile.id, "balance_netbo":balance_netboo, "created_at":taim}
+        ser = MoneyOutserialazer(data=data)
+        ser.is_valid(raise_exception=True)
+        ser.save()
+        profile.balance_netbo -= balance_netboo
+        profile.save()
+        return Response({'message': 1,"data":ser.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1,}, status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(methods='GET')
+@api_view(['GET'])
+def get_moneyout_id(request, pk):
+    if request.method == 'GET':
+        moneyouts = MoneyOut.objects.filter(user=pk)
+        serializer = MoneyOutserialazer(moneyouts, many=True)
+        return Response({'message': 1, "data": serializer.data}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': -1}, status=status.HTTP_400_BAD_REQUEST)
+    
+@swagger_auto_schema(methods='GET')
+@api_view(['GET'])
+def get_strength(request):
+    strength = Strength.objects.get(id=1)
+    serializer = StrengthSerialazer(strength)
+    return Response({'message': 1,"strength":serializer.data}, status=status.HTTP_200_OK)
+
+# @swagger_auto_schema(methods='GET')
+# @api_view(['GET'])
+# def loadjson(request):
+#     if request.method == 'GET':
+#         file_path = '/Users/davlatovbarot/Desktop/Projects/MNG/mainapp/profile_data12.json'
+#         with open(file_path, 'r') as f:
+#             data = json.load(f)
+#             for item in data:
+#                 email1 = item["fields"]["email"]
+#                 password1 = item["fields"]["password"]
+#                 username1 = item["fields"]["username"]
+#                 name1 = item["fields"]["name"]
+#                 surname1 = item["fields"]["surname"]
+#                 referal_link1 = item["fields"]["referal_link"]
+#                 number_people1 = item["fields"]["number_people"]
+#                 balance_netbo1 = item["fields"]["balance_netbo"]
+#                 wallet_id_netbo1 = item["fields"]["wallet_id_netbo"]
+#                 is_identified1 = item["fields"]["is_identified"]
+#                 is_verified1 = item["fields"]["is_verified"]
+#                 friend_referal_link1 = item["fields"]["friend_referal_link"]
+#                 profile = Profile(email=email1, password=password1, username=username1, name=name1, surname=surname1,referal_link=referal_link1, number_people=number_people1, balance_netbo=balance_netbo1, wallet_id_netbo=wallet_id_netbo1, is_identified=is_identified1,is_verified=is_verified1, friend_referal_link=friend_referal_link1)
+#                 profile.save()
+#             return Response({'message': 1}, status=status.HTTP_200_OK)
+#     else:
+#         return Response({'message': -1},status=status.HTTP_400_BAD_REQUEST)
